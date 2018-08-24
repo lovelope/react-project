58f196059e3eb4d4d8666d63590e7436db6c8a14
lovelope <lovelope@qq.com>
2018-08-24 10:34:34 +0800

    fea: add antd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '@/App';
+// 本地化
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+ReactDOM.render(
+  <LocaleProvider locale={zhCN}>
+    <App />
+  </LocaleProvider>,
+  document.getElementById('root')
+);
