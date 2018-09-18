dc91b7a0bf1453896f8b41a6bc831ece77ede60e
lovelope <lovelope@qq.com>
2018-09-18 20:55:42 +0800

    mod: babel

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   // plugins 加载顺序，数组从前往后
   plugins: [
-    '@babel/plugin-syntax-dynamic-import', // 支持 `() => import('./Home')` 语法
-    '@babel/plugin-transform-runtime',
     ['@babel/plugin-proposal-decorators', { legacy: true }], // 修饰器支持，顺序必须在 `proposal-class-properties` 之前
     '@babel/plugin-proposal-class-properties', // 支持 `handleChange = () => {}` 语法
+    '@babel/plugin-syntax-dynamic-import', // 支持 `() => import('./Home')` 语法
+    '@babel/plugin-transform-runtime',
     [
       'import',
       { libraryName: 'antd', libraryDirectory: 'es', style: 'css' },
@@ -15,5 +15,5 @@ module.exports = {
 
   // presets 加载顺序，数组从后往前
   // 先执行 `react`， 然后执行 `env`
-  presets: ['@babel/env', '@babel/react'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
 };
