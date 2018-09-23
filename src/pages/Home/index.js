8589d65eaae9dbfa84351316a05a6104041d06d4
lovelope <lovelope@qq.com>
2018-09-23 18:33:10 +0800

    mod: eslint config

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,20 +3,25 @@ import { DatePicker } from 'antd';
 import './index.less';
 
 class Home extends Component {
-  // constructor() {
-  //   super();
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  handleChange = (date, dateString) => {
-    console.log(date, dateString);
-  };
+  handleChange(date, dateString) {
+    console.info(date, dateString);
+    this.setState({ value: date });
+  }
 
   render() {
+    const { value } = this.state;
     return (
       <div className="page-home">
         <h1>Home</h1>
-        <DatePicker onChange={this.handleChange} />
+        <DatePicker value={value} onChange={this.handleChange} />
       </div>
     );
   }
