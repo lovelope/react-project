8f382addda723d4cba2396d1d57ac54376e92318
lovelope <lovelope@qq.com>
2018-10-17 13:54:28 +0800

    reconf: 重构 styleLoaders

---
+++ tools/getStyleLoaders.js
@@ -0,0 +1,88 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+/**
+ * 获取 cssLoaders
+ * @param {*} param0
+ */
+export default function getStyleLoaders({
+  isProd = true, // 生产环境
+  sourceMap = true, // 打开 sourceMap
+  modules = false, // 开启 css module
+  useLess = false, // 使用 less
+  modifyVars = {}, // 修改 less 变量
+}) {
+  const prodLoaders = [
+    MiniCssExtractPlugin.loader,
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap,
+
+        modules,
+
+        // 生产环境使用短类名
+        localIdentName: '[local]--[hash:base64:8]',
+
+        // 前置loader数量1，postcss-loader
+        importLoaders: useLess ? 2 : 1,
+      },
+    },
+    {
+      loader: 'postcss-loader',
+      options: { sourceMap },
+    },
+  ].concat(
+    !useLess
+      ? []
+      : [
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars,
+              sourceMap,
+            },
+          },
+        ]
+  );
+
+  const devLoaders = [
+    {
+      loader: 'style-loader',
+      options: { sourceMap },
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap,
+
+        modules,
+
+        // 开发环境使用详细类名
+        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+
+        // 前置loader数量1，postcss-loader
+        importLoaders: useLess ? 1 : 0,
+      },
+    },
+  ].concat(
+    !useLess
+      ? []
+      : [
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars,
+              sourceMap,
+            },
+          },
+        ]
+  );
+
+  if (isProd) {
+    return prodLoaders;
+  }
+  return devLoaders;
+}
