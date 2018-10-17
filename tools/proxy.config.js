0c010c18f7bce8c58ef3c66232c0c80a3b9311bc
lovelope <lovelope@qq.com>
2018-10-17 12:40:39 +0800

    proxy

--- tools/proxy.config.js
+++ tools/proxy.config.js
@@ -1,11 +1,20 @@
+// 外网 IP ，欺瞒服务端 ip 验证
+const XForwardedFor = '123.234.123.234';
+
+const headers = {
+  'X-Forwarded-For': XForwardedFor,
+};
+
 export default {
   mock: {
     '/api3': {
       changeOrigin: true,
+      headers,
       target: 'http://api3.mock.server.com',
     },
     '/api': {
       changeOrigin: true,
+      headers,
       target: 'http://api.mock.server.com',
       pathRewrite: {
         '^/api': '/api2/write/',
@@ -15,10 +24,12 @@ export default {
   dev: {
     '/api3': {
       changeOrigin: true,
+      headers,
       target: 'http://api3.dev.server.com',
     },
     '/api': {
       changeOrigin: true,
+      headers,
       target: 'http://api.dev.server.com',
       pathRewrite: {
         '^/api': '/api2/write/',
@@ -28,10 +39,12 @@ export default {
   qa: {
     '/api3': {
       changeOrigin: true,
+      headers,
       target: 'http://api3.qa.server.com',
     },
     '/api': {
       changeOrigin: true,
+      headers,
       target: 'http://api.qa.server.com',
       pathRewrite: {
         '^/api': '/api2/write/',
@@ -41,10 +54,12 @@ export default {
   online: {
     '/api3': {
       changeOrigin: true,
+      headers,
       target: 'http://api3.server.com',
     },
     '/api': {
       changeOrigin: true,
+      headers,
       target: 'http://api.server.com',
       pathRewrite: {
         '^/api': '/api2/write/',
