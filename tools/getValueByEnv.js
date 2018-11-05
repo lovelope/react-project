95ca07527f4804c0976b60f29098b9b48c242841
lovelope <lovelope@qq.com>
2018-11-05 17:18:00 +0800

    style: 移除冗余注释

--- tools/getValueByEnv.js
+++ tools/getValueByEnv.js
@@ -1,12 +1,6 @@
 /**
  * 根据环境变量获取相应值
- * 根据 npm start --[参数] 进行环境切换, 例如 `npm start --qa` 或者 `npm run build -- online`
- * @param {*} obj 对象
- * @param {*} field key 值
- */
-
-/**
- * 根据 npm start --[参数] 进行环境切换, 例如 `npm start --qa` 或者 `npm run build -- online`
+ * 根据 npm start --[参数] 进行环境切换, 例如 `npm start --qa` 或者 `npm run build --online`
  * @param {Object} obj 对象
  * @param {Object} param1 { @param {String} field key 值, @param {String} defaultEnv 默认环境}
  */
