e5d7d8a9c8a05cb73c6c60f04c254624704ca7c0
lovelope <lovelope@qq.com>
2020-08-13 11:11:11 +0800

    chore: stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,53 +1,15 @@
-// stylelint configuration
-// https://stylelint.io/user-guide/configuration/
 module.exports = {
-  // The standard config based on a handful of CSS style guides
-  // https://github.com/stylelint/stylelint-config-standard
-  extends: 'stylelint-config-standard',
-
-  plugins: [
-    // stylelint plugin to sort CSS rules content with specified order
-    // https://github.com/hudochenkov/stylelint-order
-    'stylelint-order',
-  ],
-
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-order', 'stylelint-config-rational-order/plugin'],
   rules: {
-    'property-no-unknown': [
-      true,
-      {
-        ignoreProperties: [
-          // CSS Modules composition
-          // https://github.com/css-modules/css-modules#composition
-          'composes',
-        ],
-      },
-    ],
-
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: [
-          // CSS Modules :global scope
-          // https://github.com/css-modules/css-modules#exceptions
-          'global',
-          'local',
-        ],
-      },
-    ],
-
-    // Opinionated rule, you can disable it if you want
-    'string-quotes': 'single',
-
-    // https://github.com/hudochenkov/stylelint-order/blob/master/rules/order/README.md
-    'order/order': [
-      'custom-properties',
-      'dollar-variables',
-      'declarations',
-      'at-rules',
-      'rules',
-    ],
-
-    // https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-order/README.md
+    'declaration-empty-line-before': null,
     'order/properties-order': [],
+    'plugin/rational-order': [
+      true,
+      {
+        'border-in-box-model': false,
+        'empty-line-between-groups': true,
+      },
+    ],
   },
 };
