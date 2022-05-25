28f0139dc08c779540cf394005a5b45f83fe6786
lovelope <lovelope@qq.com>
2022-05-25 10:31:48 +0800

    fix: dev server does not match the API schema

--- tools/webpack.config.development.babel.ts
+++ tools/webpack.config.development.babel.ts
@@ -8,11 +8,11 @@ import type { Configuration as WebpackConfiguration } from 'webpack';
 import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
 import webpackConfigBase from './webpack.config.base.babel';
-import paths, { PUBLIC_PATH } from './paths';
+import paths from './paths';
 import proxyConfigAll from './proxy.config';
 import getValueByEnv from './getValueByEnv';
 
-const HOST = process.env.HOST || '0.0.0.0'; // 本机IP
+const HOST = process.env.HOST || '127.0.0.1'; // 本机IP
 const PORT = process.env.PORT || 8080; // 端口号
 
 const proxyCurrent = getValueByEnv(proxyConfigAll, { defaultEnv: 'dev' });
@@ -26,17 +26,8 @@ const webpackConfigDev: Configuration = merge(webpackConfigBase, {
     // 允许访问的机器列表
     allowedHosts: [HOST, 'localhost', '127.0.0.1'],
 
-    // 客户端日志信息等级
-    clientLogLevel: 'none',
-
     // gzip 压缩
     compress: true,
-
-    // 提供静态资源的文件夹，可以使用数组，推荐使用 *绝对路径*
-    contentBase: paths.appPublic,
-
-    // 绕过主机检查，仅在需要其它机器连接时打开此选项
-    disableHostCheck: false,
 
     // html5路由
     historyApiFallback: true,
@@ -55,12 +46,6 @@ const webpackConfigDev: Configuration = merge(webpackConfigBase, {
 
     // api转发
     proxy: proxyCurrent,
-
-    // 服务目录，总是以`/`开头或结尾，与 `output.publicPath` 相同
-    publicPath: PUBLIC_PATH,
-
-    // 此选项允许浏览器使用本地 IP 打开。
-    useLocalIp: true,
   },
   plugins: [
     // html 模板
